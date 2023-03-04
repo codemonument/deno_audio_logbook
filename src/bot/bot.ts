8ca7f8d92ff295bb324b40a1e5ea69f9a7406dec
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-04 19:49:17 +0100

    generate a new voice message reply!

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -19,9 +19,20 @@ async function initBot() {
     bot.command("start", (ctx) => ctx.reply("Welcome! Send me a photo!"));
     bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo!"));
     bot.on("message:photo", (ctx) => ctx.reply("Nice photo! Is that you?"));
-    bot.on("message:audio", (ctx) => {
-        console.log(ctx);
-        ctx.reply("Received audio")
+    bot.on("message:voice", (ctx) => {
+        const voice = ctx.message.voice;
+
+        // Extract voice object -  https://core.telegram.org/bots/api#voice
+        const reply = ` Got a new Voice Message! 
+        Duration: ${voice.duration},
+        File ID: ${voice.file_id},
+        File Size: ${voice.file_size},
+        File Unique Id: ${voice.file_unique_id},
+        File Mime Type: ${voice.mime_type},
+        Full Voice Object.toString(): ${voice.toString()},
+        `
+
+        ctx.reply(reply);
     });
     bot.on(
         "edited_message",
