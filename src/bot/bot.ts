c40926dd4d29beba92356d4b2c28513de98926f6
Bloodiko <bastian.jesuiter@gmail.com>
2023-03-04 20:56:05 +0100

    add userID to response on voice

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,53 +1,54 @@
 import { Bot } from "grammy";
-import { secretsPromise } from '@/src/secrets.ts';
+import { secretsPromise } from "@/src/secrets.ts";
 import { z } from "zod";
 
 // Create bot object
 export const botPromise = initBot();
 
 async function initBot() {
-    const secrets = await secretsPromise;
-    const telegramToken = secrets.get('TELEGRAM_TOKEN');
+  const secrets = await secretsPromise;
+  const telegramToken = secrets.get("TELEGRAM_TOKEN");
 
-    const bot = new Bot(z.string().parse(telegramToken));
+  const bot = new Bot(z.string().parse(telegramToken));
 
-    // Register Webhook
-    await fetch(`https://api.telegram.org/bot${telegramToken}/setWebhook?url=https://deno-audio-logbook.deno.dev/bot/${telegramToken}`);
+  // Register Webhook
+  await fetch(
+    `https://api.telegram.org/bot${telegramToken}/setWebhook?url=https://deno-audio-logbook.deno.dev/bot/${telegramToken}`,
+  );
 
+  // Listen for messages
+  bot.command("start", (ctx) => ctx.reply("Welcome! Send me a photo!"));
+  bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo!"));
+  bot.on("message:photo", (ctx) => ctx.reply("Nice photo! Is that you?"));
+  bot.on("message:voice", (ctx) => {
+    const voice = ctx.message.voice;
 
-    // Listen for messages
-    bot.command("start", (ctx) => ctx.reply("Welcome! Send me a photo!"));
-    bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo!"));
-    bot.on("message:photo", (ctx) => ctx.reply("Nice photo! Is that you?"));
-    bot.on("message:voice", (ctx) => {
-        const voice = ctx.message.voice;
-
-        // Extract voice object -  https://core.telegram.org/bots/api#voice
-        const reply = ` Got a new Voice Message! 
+    // Extract voice object -  https://core.telegram.org/bots/api#voice
+    const reply = ` Got a new Voice Message! 
         Duration: ${voice.duration},
         File ID: ${voice.file_id},
         File Size: ${voice.file_size},
         File Unique Id: ${voice.file_unique_id},
         File Mime Type: ${voice.mime_type},
         Full Voice Object.toString()
-        ${JSON.stringify(voice, undefined, '\t')},
-        `
+        ${JSON.stringify(voice, undefined, "\t")},
+        UserID: ${ctx.message.from.id},
+        `;
 
-        // Download the voice file via: https://core.telegram.org/bots/api#getfile
+    // Download the voice file via: https://core.telegram.org/bots/api#getfile
 
-        ctx.reply(reply);
-    });
-    bot.on(
-        "edited_message",
-        (ctx) =>
-            ctx.reply("Ha! Gotcha! You just edited this!", {
-                reply_to_message_id: ctx.editedMessage.message_id,
-            }),
-    );
+    ctx.reply(reply);
+  });
+  bot.on(
+    "edited_message",
+    (ctx) =>
+      ctx.reply("Ha! Gotcha! You just edited this!", {
+        reply_to_message_id: ctx.editedMessage.message_id,
+      }),
+  );
 
-    return bot;
+  return bot;
 }
-
 
 // Not Launch, pass to webhook adapter instead
 // bot.start();
