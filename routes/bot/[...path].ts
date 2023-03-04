133c97f5111c4a0f2673f2fa7bcf0cf53b9aafe9
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-04 18:31:03 +0100

    try to integrate the bot as webhookCallback (WIP)

--- routes/bot/[...path].ts
+++ routes/bot/[...path].ts
@@ -1,10 +1,21 @@
 import { HandlerContext } from "$fresh/server.ts";
+import { webhookCallback } from "grammy";
+import { botPromise } from "@/src/bot/bot.ts";
 
 /**
  * The adapter path for the telegram bot
+ * 
+ * How to use grammy webhooks: 
+ * https://grammy.dev/guide/deployment-types.html#how-to-use-webhooks
  */
 
-export const handler = (_req: Request, ctx: HandlerContext): Response => {
+
+export const handler = async (_req: Request, ctx: HandlerContext): Promise<Response> => {
     const path = ctx.params.path;
+
+    const bot = await botPromise;
+    const result = webhookCallback(bot, "callback");
+    console.log(result);
+
     return new Response(path);
 };
