1d42c8c2621d66a0740950ae98f32fb22065acb3
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-04 18:30:45 +0100

    add grammy bot setup

---
+++ src/bot/bot.ts
@@ -0,0 +1,32 @@
+import { Bot } from "grammy";
+import { secretsPromise } from '@/src/secrets.ts';
+import { z } from "zod";
+
+// Create bot object
+export const botPromise = initBot();
+
+async function initBot() {
+    const secrets = await secretsPromise;
+    const telegramToken = secrets.get('TELEGRAM_TOKEN');
+
+    const bot = new Bot(z.string().parse(telegramToken));
+
+
+    // Listen for messages
+    bot.command("start", (ctx) => ctx.reply("Welcome! Send me a photo!"));
+    bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo!"));
+    bot.on("message:photo", (ctx) => ctx.reply("Nice photo! Is that you?"));
+    bot.on(
+        "edited_message",
+        (ctx) =>
+            ctx.reply("Ha! Gotcha! You just edited this!", {
+                reply_to_message_id: ctx.editedMessage.message_id,
+            }),
+    );
+
+    return bot;
+}
+
+
+// Not Launch, pass to webhook adapter instead
+// bot.start();
