3beafc92139d01459167a1fd642e8c1dce81fd4b
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 23:16:35 +0100

    make Deployment_ID show "unkonwn/unavailable" when running at localhost

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-
 export const AUDIO_LOGBOOK_AUTH_COOKIE_NAME = "audio_logbook_auth";
-export const DEPLOYMENT_ID = Deno.env.get("DENO_DEPLOYMENT_ID") ?? undefined;
+export const DEPLOYMENT_ID = Deno.env.get("DENO_DEPLOYMENT_ID") ??
+  "unknown/unavailable";
