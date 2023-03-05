c6ce1da52fbf74b65a91a0c25781c3ca333686fb
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 17:46:58 +0100

    reformat prefillCssCache and fix logger name

--- src/css-cache/prefillCssCache.ts
+++ src/css-cache/prefillCssCache.ts
@@ -5,53 +5,50 @@ import postcssJitProps from "postcss-jit-props";
 import OpenProps from "open-props";
 import postcssImport from "postcss-import";
 import { log } from "@/src/log.ts";
-import { expandGlob, WalkEntry } from "std_fs"
+import { expandGlob, WalkEntry } from "std_fs";
 import { encode as encodeBase64 } from "std_encoding_base64";
 import { cssCache } from "./cssCache.ts";
 
 // FIXME: Why does this logger not log at the start?!?
-const logger = log.getLogger("openprops-in-fresh");
+const logger = log.getLogger("deno_audio_logbook");
 
 export const postcssInstance = postcss([
-    postcssImport({
-        addModulesDirectories: ['css_deps']
-    }),
-    postcssJitProps(OpenProps),
+  postcssImport({
+    addModulesDirectories: ["css_deps"],
+  }),
+  postcssJitProps(OpenProps),
 ]);
 
-
 async function loadProcessAndCacheCss(file: WalkEntry) {
-    const fsPath = file.path;
+  const fsPath = file.path;
 
-    // Load input css file
-    const rawCssContent = await Deno.readTextFile(fsPath);
+  // Load input css file
+  const rawCssContent = await Deno.readTextFile(fsPath);
 
-    // Calc SHA256
-    const rawCssBytes = new TextEncoder().encode(rawCssContent);
-    const fileHashBytes = await crypto.subtle.digest('SHA-256', rawCssBytes);
-    const fileHash = encodeBase64(fileHashBytes);
+  // Calc SHA256
+  const rawCssBytes = new TextEncoder().encode(rawCssContent);
+  const fileHashBytes = await crypto.subtle.digest("SHA-256", rawCssBytes);
+  const fileHash = encodeBase64(fileHashBytes);
 
-    const processingResult = await postcssInstance.process(rawCssContent, {
-        from: fsPath,
-    });
-    cssCache.set(fileHash, processingResult.css)
+  const processingResult = await postcssInstance.process(rawCssContent, {
+    from: fsPath,
+  });
+  cssCache.set(fileHash, processingResult.css);
 
-    console.debug(`PostCSS Transformed and Cached: ${fsPath}`, { fileHash });
+  console.debug(`PostCSS Transformed and Cached: ${fsPath}`, { fileHash });
 }
 
 /**
  * Process css files and store them in css cache at server start
  */
 export async function prefillCssCache() {
+  const cssFileEntries = expandGlob("css/*.css", { root: Deno.cwd() });
+  for await (const file of cssFileEntries) {
+    await loadProcessAndCacheCss(file);
+  }
 
-    const cssFileEntries = expandGlob('css/*.css', { root: Deno.cwd() });
-    for await (const file of cssFileEntries) {
-        await loadProcessAndCacheCss(file)
-    }
-
-    const cssStaticEntries = expandGlob('static/*.css', { root: Deno.cwd() });
-    for await (const file of cssStaticEntries) {
-        await loadProcessAndCacheCss(file);
-    }
+  const cssStaticEntries = expandGlob("static/*.css", { root: Deno.cwd() });
+  for await (const file of cssStaticEntries) {
+    await loadProcessAndCacheCss(file);
+  }
 }
-
