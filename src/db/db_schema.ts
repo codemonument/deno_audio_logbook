a5a59364fb69a4e670bf47961330f0200ef25d94
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 20:27:30 +0100

    refine UserTable to SessionTable

--- src/db/db_schema.ts
+++ src/db/db_schema.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export type AudioRecordingsTable = {
   /**
    * Telegram Voice/Audio File ID
@@ -28,12 +30,35 @@ export type AudioRecordingsTable = {
   unixTimestamp: number;
 };
 
-export type UserTable = {
-  // is number good? => forgeability
-  userId: number;
-};
+export const SessionTable = z.object({
+  /**
+   * The hash of this one loggin request 
+   * Will be the primary key for this table 
+   */
+  hash: z.string(),
+
+  /**
+   * Telegram User Id
+   */
+  userId: z.number(),
+
+  /**
+   * The unix auth date timestamp
+   */
+  unixAuthDate: z.number(),
+
+  /**
+   * Some user information for pretty display
+   */
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  username: z.string().optional(),
+  photoUrl: z.string().optional(),
+});
+
+export type SessionTable = z.infer<typeof SessionTable>;
 
 export type DbSchema = {
   audiobook_recordings: AudioRecordingsTable;
-  audiobook_users: UserTable;
+  audiobook_sessions: SessionTable;
 };
