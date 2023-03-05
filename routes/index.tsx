420f8fc748d1ad878c2bd6d2117e473ac6d44f54
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 19:50:27 +0100

    output deno deployment id on index.ts

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,8 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
 
+const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID") ?? undefined;
+
 export default function Home() {
   return (
     <>
@@ -8,6 +10,7 @@ export default function Home() {
         <title>Audio Logbook</title>
       </Head>
       <div>
+        <pre>Deno Deployment ID{deploymentId}</pre>
         <h1>Audio Logbook</h1>
         
         <script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="audio_logbook_bot" data-size="large" data-auth-url="/auth/callback" data-request-access="write"></script>
