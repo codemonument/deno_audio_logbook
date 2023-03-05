1680edcfafd9d9b1559b32678689c37166ad49d2
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-05 21:13:04 +0100

    add redirect to login page, if no token is available, render user data, if available

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,19 +1,63 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
+import { getCookies, setCookie } from "$std/http/cookie.ts";
+import {
+  AUDIO_LOGBOOK_AUTH_COOKIE_NAME,
+  DEPLOYMENT_ID,
+} from "@/src/constants.ts";
+import { dbPromise } from "@/src/db/db.ts";
+import { UserSession } from "@/src/db/db_schema.ts";
 
-const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID") ?? undefined;
+export async function handler(
+  req: Request,
+  ctx: HandlerContext,
+): Promise<Response> {
+  // Get cookie from request header and parse it
+  const maybeAccessToken =
+    getCookies(req.headers)[AUDIO_LOGBOOK_AUTH_COOKIE_NAME];
+  const db = await dbPromise;
 
-export default function Home() {
+  if (maybeAccessToken) {
+    const maybeUser = await db.selectFrom("audiobook_sessions").where(
+      "hash",
+      "=",
+      maybeAccessToken,
+    ).executeTakeFirst();
+    const user = UserSession.safeParse(maybeUser);
+
+    if (user.success) {
+      return ctx.render({ user: user.data });
+    }
+  }
+
+  return new Response("", {
+    headers: new Headers(
+      [
+        ["location", new URL(req.url).origin + "/auth/login"],
+      ],
+    ),
+  });
+}
+
+export default function Home({ user }: { user: UserSession }) {
   return (
     <>
       <Head>
         <title>Audio Logbook</title>
       </Head>
       <div>
-        <pre>Deno Deployment ID{deploymentId}</pre>
+        <pre>Deno Deployment ID: {DEPLOYMENT_ID}</pre>
         <h1>Audio Logbook</h1>
-        
-        <script async src="https://telegram.org/js/telegram-widget.js?21" data-telegram-login="audio_logbook_bot" data-size="large" data-auth-url="/auth/callback" data-request-access="write"></script>
+
+        <p>
+          <b>Hello, {user.firstName} {user.lastName}!</b>
+          <img src={user.photoUrl}></img>
+        </p>
+
+        <ul>
+          <li>{user.username}</li>
+          <li>{user.unixAuthDate}</li>
+        </ul>
       </div>
     </>
   );
