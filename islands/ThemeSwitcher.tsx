206ce9f5cef96ca50534508b47e56520a0f14969
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-10 21:05:22 +0100

    add rudimentary theme switcher

--- islands/ThemeSwitcher.tsx
+++ islands/ThemeSwitcher.tsx
@@ -1,25 +1,25 @@
 import { PageProps } from "$fresh/server.ts";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
+export default function ThemeSwitcher(props: { selected: string }) {
+  let doc: Element | null;
 
-type ThemeSwitcherProp = {
-  selected: string;
-};
+  if (IS_BROWSER) {
+    doc = document.firstElementChild;
+  }
 
-export default function ThemeSwitcher(props: PageProps<ThemeSwitcherProp>) {
-    let doc: Element | null; 
+  const changeTheme = (theme: string) => {
+    doc?.setAttribute(`color-scheme`, theme);
+  };
 
-    if (IS_BROWSER) {
-        doc = document.firstElementChild;
-    }
-
-    const changeTheme = (event: Event) => {
-        const theme = (event.target as HTMLSelectElement).value;
-        doc?.setAttribute(`color-scheme`, theme);
-      };
+  changeTheme(props.selected);
 
   return (
-    <select name="theme" id="theme-switcher" onChange={changeTheme}>
+    <select
+      name="theme"
+      id="theme-switcher"
+      onChange={(e) => changeTheme((e.target as HTMLSelectElement).value)}
+    >
       <option value="light" selected>Light 🌞</option>
       <option value="dark">Dark 🌙</option>
     </select>
