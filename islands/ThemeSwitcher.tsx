273493c70e18a45bd8bcd934f3a2b7f39e7a198e
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-11 20:03:52 +0100

    simplify theme switcher by not using preact state

--- islands/ThemeSwitcher.tsx
+++ islands/ThemeSwitcher.tsx
@@ -21,20 +21,24 @@ export default function ThemeSwitcher() {
   // Anything here runs on the client!
   // Important: AND RE-RUNS when useState hook changes!!!
 
-   // Halts in chrome
+  // Halts in chrome
   debugger;
 
   // get htmlTag
-  const [htmlTag] = useState(document.firstElementChild);
+  const htmlTag = document.firstElementChild;
   if (!htmlTag) throw new Error(`<html> tag could not be found!`);
 
   const storedTheme = localStorage.getItem("theme")?.trim() ?? "";
-  const [theme, setTheme] = useState(storedTheme);
 
-  // applies the theme inside the state var 'theme' to the html tag
-  useEffect(() => htmlTag?.setAttribute(`theme`, theme), [theme]);
+  // applies the theme to the html tag
+  const changeTheme = (newTheme: string) => {
+    htmlTag?.setAttribute(`theme`, newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
-  if (storedTheme === "") {
+  let theme = storedTheme;
+
+  if (theme === "") {
     // read system color scheme
     const systemColorScheme = getComputedStyle(htmlTag)
       .getPropertyValue("--system-color-scheme").trim();
@@ -43,17 +47,18 @@ export default function ThemeSwitcher() {
     localStorage.setItem("theme", systemColorScheme);
 
     // apply the system color scheme theme
-    setTheme(systemColorScheme);
-    // Stop Rendering with theme === ""
-    return; 
+    theme = systemColorScheme;
   }
+
+  // apply theme
+  changeTheme(theme);
 
   return (
     <select
       name="theme"
       id="theme-switcher"
       value={theme}
-      onChange={(e) => setTheme((e.target as HTMLSelectElement).value)}
+      onChange={(e) => changeTheme((e.target as HTMLSelectElement).value)}
     >
       <option value="light">Light 🌞</option>
       <option value="dark">Dark 🌙</option>
