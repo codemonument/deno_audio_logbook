bc1801547b2c44bc53e1455e75107dd86b15a501
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-15 23:23:30 +0100

    add root middleware to figure out serverOrigin

---
+++ routes/_middleware.ts
@@ -0,0 +1,18 @@
+import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import { serverOrigin } from "../src/server_constants.ts";
+
+let originInitialized = false;
+
+export async function handler(
+  req: Request,
+  ctx: MiddlewareHandlerContext<void>,
+) {
+  if (!originInitialized) {
+    const origin = new URL(req.url).origin;
+    serverOrigin.resolve(origin);
+    originInitialized = true;
+  }
+
+  const resp = await ctx.next();
+  return resp;
+}
