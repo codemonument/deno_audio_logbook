0454eae64811011b191324c96a0a835b55f7afc3
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-16 00:09:43 +0100

    add UserSession loading in route middleware

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,11 +1,23 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
-import { serverOrigin } from "@/src/const/server_constants.ts";
+import {
+  AUDIO_LOGBOOK_AUTH_COOKIE_NAME,
+  DEPLOYMENT_ID,
+  serverOrigin,
+} from "@/src/const/server_constants.ts";
+import { UserSession } from "@/src/db/db_schema.ts";
+import { secretsPromise } from "@/src/utils/secrets.ts";
+import { getCookies } from "$std/http/cookie.ts";
+import { dbPromise } from "@/src/db/db.ts";
 
 let originInitialized = false;
 
+export type RootMiddlewareState = {
+  user?: UserSession;
+};
+
 export async function handler(
   req: Request,
-  ctx: MiddlewareHandlerContext<void>,
+  ctx: MiddlewareHandlerContext<RootMiddlewareState>,
 ) {
   if (!originInitialized) {
     const origin = new URL(req.url).origin;
@@ -13,6 +25,35 @@ export async function handler(
     originInitialized = true;
   }
 
-  const resp = await ctx.next();
-  return resp;
+  // When in dev mode,
+  // - get auth token from doppler secrets for local mocking, or
+  // - get auth token from request header in anything other than dev mode
+  // - and parse it
+  const url = new URL(req.url);
+  const secrets = await secretsPromise;
+  const maybeAccessToken =
+    (secrets.get("ENV_NAME") === "dev" || url.host.includes(DEPLOYMENT_ID))
+      ? secrets.get("MOCK_AUTH_TOKEN")
+      : getCookies(req.headers)[AUDIO_LOGBOOK_AUTH_COOKIE_NAME];
+
+  if (maybeAccessToken) {
+    const db = await dbPromise;
+    const maybeUserQuery = db
+      .selectFrom("audiobook_sessions")
+      .selectAll()
+      .where(
+        "hash",
+        "=",
+        maybeAccessToken,
+      );
+    const maybeUser = await maybeUserQuery.executeTakeFirst();
+    const userParse = UserSession.safeParse(maybeUser);
+
+    if (userParse.success) {
+      ctx.state.user = userParse.data;
+    }
+
+    const resp = await ctx.next();
+    return resp;
+  }
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,46 +1,13 @@
-import { HandlerContext } from "$fresh/server.ts";
-import { getCookies } from "$std/http/cookie.ts";
-import {
-  AUDIO_LOGBOOK_AUTH_COOKIE_NAME,
-  DEPLOYMENT_ID,
-} from "@/src/const/server_constants.ts";
-import { dbPromise } from "@/src/db/db.ts";
-import { UserSession } from "@/src/db/db_schema.ts";
-import { secretsPromise } from "@/src/utils/secrets.ts";
+import { Handlers } from "$fresh/server.ts";
 import { gotoCalendar, gotoLogin } from "@/src/utils/redirects.ts";
+import type { RootMiddlewareState } from "./_middleware.ts";
 
-export async function handler(
-  req: Request,
-  ctx: HandlerContext,
-): Promise<Response> {
-  // When in dev mode,
-  // - get auth token from doppler secrets for local mocking, or
-  // - get auth token from request header in anything other than dev mode
-  // - and parse it
-  const url = new URL(req.url);
-  const secrets = await secretsPromise;
-  const maybeAccessToken =
-    (secrets.get("ENV_NAME") === "dev" || url.host.includes(DEPLOYMENT_ID))
-      ? secrets.get("MOCK_AUTH_TOKEN")
-      : getCookies(req.headers)[AUDIO_LOGBOOK_AUTH_COOKIE_NAME];
-
-  if (maybeAccessToken) {
-    const db = await dbPromise;
-    const maybeUserQuery = db
-      .selectFrom("audiobook_sessions")
-      .selectAll()
-      .where(
-        "hash",
-        "=",
-        maybeAccessToken,
-      );
-    const maybeUser = await maybeUserQuery.executeTakeFirst();
-    const user = UserSession.safeParse(maybeUser);
-
-    if (user.success) {
+export const handler: Handlers<unknown, RootMiddlewareState> = {
+  GET(_req, ctx) {
+    if (ctx.state.user) {
       return gotoCalendar();
     }
-  }
 
-  return gotoLogin();
-}
+    return gotoLogin();
+  },
+};
