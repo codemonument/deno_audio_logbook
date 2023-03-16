3ae9a78c355f2302d626ee4b7cbea4c5e9db91d2
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-16 01:15:03 +0100

    make data-parsing error more beautiful

--- routes/errors/date-parsing.tsx
+++ routes/errors/date-parsing.tsx
@@ -1,20 +1,41 @@
 import { MONTH_NUMBER_STRING } from "@/src/const/client_constants.ts";
+import Layout from "@/components/Layout.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { ContextState } from "@/src/context_state.ts";
+import { UserSession } from "@/src/db/db_schema.ts";
 
-export default function DateParsingErrorPage() {
+/**
+ * @requires ctx.state.user => The user session, resolved by the _middleware.ts file
+ */
+export const handler: Handlers<unknown, ContextState> = {
+  GET(_req, ctx) {
+    // Render calendar with audio and user objects
+    return ctx.render({ user: ctx.state.user });
+  },
+};
+
+export default function DateParsingErrorPage(
+  props: PageProps<{ user: UserSession }>,
+) {
   const currentMonth = MONTH_NUMBER_STRING[new Date().getMonth()];
   const currentYear = new Date().getFullYear().toString();
 
   return (
-    <>
-      <p style="background-color: red;">
-        Oooops! You passed a invalid date, so the calendar cannot be shown!
-        Please specify a year &gt; 1970 and a month between 01 - 12, or press
-        the button below to go back to the callendar with the current month!
-      </p>
-
-      <a href={`/calendar/${currentYear}/${currentMonth}`}>
-        Go back to Calendar with current month
-      </a>
-    </>
+    <Layout user={props.data.user}>
+      <div class="error medium-width">
+        Oooops! You passed a invalid date, so the calendar cannot be shown!{" "}
+        <br /> <br />
+        Please specify ...
+        <ul>
+          <li>a year &gt; 1970</li>
+          <li>and a month between 01 - 12</li>
+        </ul>
+        <br />
+        ...or &nbsp;
+        <a href={`/calendar/${currentYear}/${currentMonth}`}>
+          Go back to Calendar with current month
+        </a>
+      </div>
+    </Layout>
   );
 }
