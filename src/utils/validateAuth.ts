2b394d7f01f207635f9a152aa9673cd5bdcad99b
Bloodiko <bastian.jesuiter@gmail.com>
2023-03-26 15:22:45 +0200

    extract UserSession from middleware

---
+++ src/utils/validateAuth.ts
@@ -0,0 +1,32 @@
+import { secretsPromise } from "./secrets.ts";
+import {
+  AUDIO_LOGBOOK_AUTH_COOKIE_NAME,
+  DEPLOYMENT_ID,
+} from "@/src/const/server_constants.ts";
+
+import { getCookies } from "$std/http/cookie.ts";
+import { getUserSession } from "@/src/db/db_queries.ts";
+import { None } from "optionals";
+import { UserSession } from "@/src/db/db_schema.ts";
+
+const secrets = await secretsPromise;
+
+export async function validateAuth(req: Request) {
+  // When in dev mode,
+  // - get auth token from doppler secrets for local mocking, or
+  // - get auth token from request header in anything other than dev mode
+  // - and parse it
+  const url = new URL(req.url);
+
+  const maybeAccessToken =
+    (secrets.get("ENV_NAME") === "dev" || url.host.includes(DEPLOYMENT_ID))
+      ? secrets.get("MOCK_AUTH_TOKEN")
+      : getCookies(req.headers)[AUDIO_LOGBOOK_AUTH_COOKIE_NAME]; //return undefined if no cookie is set
+
+  if (!maybeAccessToken) {
+    return None<UserSession>();
+  }
+  const userSession = await getUserSession(maybeAccessToken);
+
+  return userSession;
+}
