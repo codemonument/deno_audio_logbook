3ffd76ad797419286d19cb775d7a1a9b77096096
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-03 22:26:00 +0200

    update bot script in TelegramLogin component

--- islands/TelegramLogin.tsx
+++ islands/TelegramLogin.tsx
@@ -3,13 +3,25 @@ export default function TelegramLogin(props: { telegramBotUser: string }) {
     <div class="login-card">
       <script
         async
+        src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login={props.telegramBotUser}
+        data-size="large"
+        data-auth-url="/auth/callback"
+      >
+      </script>
+
+      {/* OLD BOT Script - remove when new one works in master! */}
+      {
+        /* <script
+        async
         src="https://telegram.org/js/telegram-widget.js?21"
         data-telegram-login={props.telegramBotUser}
         data-size="large"
         data-auth-url="/auth/callback"
         data-request-access="write"
       >
-      </script>
+      </script> */
+      }
     </div>
   );
 }
