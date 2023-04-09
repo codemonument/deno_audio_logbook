78461acd5639363fe78bd16c8c59990db9b12243
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-09 21:10:38 +0200

    add util function: getThemeOnServer

---
+++ src/utils/getThemeOnServer.ts
@@ -0,0 +1,12 @@
+import { getCookies } from "$std/http/cookie.ts";
+import { COOKIE_THEME } from "@/src/const/client_constants.ts";
+
+export function getThemeOnServer(req: Request) {
+  // TODO:
+  // Detect the default via client hint headers:
+  // https://dev.to/bryce/detecting-dark-mode-on-every-request-21b2
+  const defaultTheme = "dark";
+  const cookieTheme = getCookies(req.headers)[COOKIE_THEME] ?? defaultTheme;
+
+  return cookieTheme;
+}
