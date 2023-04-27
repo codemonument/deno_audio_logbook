400314beca3ff25e498c881ce6b5c56efac19317
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-27 13:26:03 +0200

    make download-openprops script to a real cliffy cli

--- scripts/download-openprops.ts
+++ scripts/download-openprops.ts
@@ -1,31 +1,50 @@
 import { ensureDir } from "https://deno.land/std@0.178.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.178.0/path/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+import {
+  ArgumentValue,
+  Command,
+} from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
+import { ZodSemver } from "https://deno.land/x/zod_semver@1.0.2/mod.ts";
 
-/**
- * Get Latest version: https://unpkg.com/open-props
- */
-const version = '1.5.5';
-const baseUrl = `https://unpkg.com/open-props@${version}`;
+await new Command()
+  .name("download-openprops")
+  .description(
+    `Downloads the specified version of openprops into a local directory for further use`,
+  )
+  .option(
+    "- --outPath <outPath>",
+    "The output path to save the openprops files to. Defaults to <cwd>/css_deps/open-props",
+  )
+  .type("semver", ({ label, name, value }: ArgumentValue) => {
+    return ZodSemver.parse(value);
+  })
+  .arguments("<openpropsVersion:semver>")
+  .action(async (options, ...args) => {
+    // Find Latest version via: https://unpkg.com/open-props
+    const [version] = args;
+    const baseUrl = `https://unpkg.com/open-props@${version}`;
+    const targetDir = options.outPath ?? `css_deps/open-props`;
 
-// Idea: provide these dependencies for postcssImport to allow compilation to work properly
-// This is the replacement for caching node_modules with open-props locally
-const targetDir = `css_deps/open-props`;
-await ensureDir(targetDir);
+    await ensureDir(targetDir);
+    const openPropsMin = await (await fetch(`${baseUrl}/open-props.min.css`))
+      .text();
+    await Deno.writeTextFile(
+      join(targetDir, "open-props.min.css"),
+      openPropsMin,
+    );
 
-const openPropsMin =
-  await (await fetch(`${baseUrl}/open-props.min.css`))
-    .text();
-await Deno.writeTextFile(join(targetDir, "open-props.min.css"), openPropsMin);
+    const normalize = await (await fetch(`${baseUrl}/normalize.min.css`))
+      .text();
+    await Deno.writeTextFile(join(targetDir, "normalize.min.css"), normalize);
 
-const normalize =
-  await (await fetch(`${baseUrl}/normalize.min.css`))
-    .text();
-await Deno.writeTextFile(join(targetDir, "normalize.min.css"), normalize);
+    const buttons = await (await fetch(`${baseUrl}/buttons.min.css`))
+      .text();
+    await Deno.writeTextFile(join(targetDir, "buttons.min.css"), buttons);
 
-const buttons =
-  await (await fetch(`${baseUrl}/buttons.min.css`))
-    .text();
-await Deno.writeTextFile(join(targetDir, "buttons.min.css"), buttons);
-
-// write version file
-await Deno.writeTextFile(join(targetDir, 'VERSION'), version)
+    // write version file
+    await Deno.writeTextFile(join(targetDir, "VERSION"), version);
+  })
+  .parse(
+    Deno.args,
+  );
